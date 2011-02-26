2664ec91abff9796698d560e9b002b977e45edfd
Daniel Ward <dwa012@gmail.com>
2011-02-26 09:14:38 -0600

    fixed conflict

--- distance.c
+++ distance.c
@@ -1,5 +1,24 @@
 #include "pib.h"
 
 double distance(particle *a, particle *b){
-    return (double)sqrt(pow(a->x - b->x, 2) + pow(a->y - b->y, 2) + pow(a->z - b->z, 2));
+
+	double deltaX = (*b).x - (*a).x;
+	double deltaY = (*b).y - (*a).y;
+	double deltaZ = (*b).z - (*a).z;
+
+    return (double)sqrt(deltaX*deltaX + deltaY*deltaY + deltaZ*deltaZ);
+}
+
+//this needs more work
+int positiveVector(particle *a, particle *b){
+	//double x = (*b).x - (*a).x ;
+	double y = (*b).y - (*a).y ;
+	//double z = (*b).z - (*a).z ;
+	int result = 0;
+	
+	
+ 	if(y > 0)
+		result = 1;
+
+	return result;
 }
