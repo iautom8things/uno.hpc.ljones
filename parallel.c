7e61e832ded9ed4a2800a207ddb1a8e6e9a88d3b
Manuel Zubieta <mazubieta@gmail.com>
2011-03-15 15:08:40 -0500

    works

--- parallel.c
+++ parallel.c
@@ -1,25 +1,29 @@
 #include "pib.h"
 
 void setup_tree (){
+    printf("started from id %d\n", id);
     srand(0); // seed the random number generator
     double random_num = rand(); //get a random number
     MPI_Status status;
     
     int max_buff_size = 4;
-    double previous_state[1];
+    int finished = 0;
+    double previous_state[4];
     
-    if (id != 1 || id != 0){
-        int max_buff_size = floor(log2(id))*4; // There's 4 double elements 
+    if (id != 1 && id != 0){
+        max_buff_size = floor(log2(id))*4; // There's 4 double elements 
         double previous_state[max_buff_size];  
     }
     if (id==0){
+        
         double temp[] = {-1, -1, -1, -1};
-        MPI_Send(temp, 4, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+        MPI_Sendrecv(&temp, 4, MPI_DOUBLE, 1, 0, &finished, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
     }
     if (id!=0){
         int parent = id/2;
-        MPI_Recv(previous_state, max_buff_size, MPI_DOUBLE, parent, parent, MPI_COMM_WORLD, &status);
-        
+        printf("%d waiting for array of size %d from %d\n", id, max_buff_size, parent);
+        MPI_Recv(&previous_state, max_buff_size, MPI_DOUBLE, parent, parent, MPI_COMM_WORLD, &status);
+        printf("%d received from %d\n", id, parent);
         // update state from passed in previous_state
             //Note: make sure we continue looking after we see a '-1'
             
@@ -31,7 +35,7 @@ void setup_tree (){
         double z = SIZE*LENGTH_OF_CUBE*((random_num)/(RAND_MAX+1.0));
         
         // copy previous_state and information from current state, to be sent to children
-        int childrens_max_buff_size = max_buff_size+4;
+        int childrens_max_buff_size = 4;
         double accepted_state[childrens_max_buff_size]; 
         double rejected_state[childrens_max_buff_size];
         
@@ -39,10 +43,13 @@ void setup_tree (){
         
         // The following can be condensed by combining the 'genereate info' portion with this
         // Written explicitly at first so as to not get ahead of myself
-        
-        for (i=0;i<max_buff_size; i++)
-            accepted_state[i] = rejected_state[i] = previous_state[i];
-            
+        if (id != 1){
+            childrens_max_buff_size = max_buff_size+4;
+            for (i=0;i<max_buff_size; i++)
+                accepted_state[i] = rejected_state[i] = previous_state[i];
+        }
+        else
+            i=0;
         accepted_state[i] = index_of_random;
         rejected_state[i++] = -1;
         accepted_state[i] = x;
@@ -55,11 +62,17 @@ void setup_tree (){
         // pass information to children
         int left_child = 2*id;
         int right_child = (2*id)+1;
-        printf("My buff: %d\tChild buff: %d\tleft_child: %d\t right: %d\n", max_buff_size, childrens_max_buff_size, left_child, right_child);
-        if (left_child < nprocs)
-            MPI_Send(accepted_state, childrens_max_buff_size, MPI_DOUBLE, left_child, id, MPI_COMM_WORLD);
-        if (right_child < nprocs)
-            MPI_Send(rejected_state, childrens_max_buff_size, MPI_DOUBLE, right_child, id, MPI_COMM_WORLD);
+         
+        if (left_child < nprocs){
+            printf("My buff: %d\tChild buff: %d\tleft_child: %d\t right: %d\n", max_buff_size, childrens_max_buff_size, left_child, right_child);
+            MPI_Sendrecv(&accepted_state, childrens_max_buff_size, MPI_DOUBLE, left_child, id, &finished, 1, MPI_INT, left_child, left_child, MPI_COMM_WORLD, &status);
+        }
+        if (right_child < nprocs){
+            printf("My buff: %d\tChild buff: %d\tleft_child: %d\t right: %d\n", max_buff_size, childrens_max_buff_size, left_child, right_child);
+            MPI_Sendrecv(&rejected_state, childrens_max_buff_size, MPI_DOUBLE, right_child, id, &finished, 1, MPI_INT, right_child, right_child, MPI_COMM_WORLD, &status);
+        }
+        int temp = 1;
+        MPI_Send(&temp, 1, MPI_INT, parent, id, MPI_COMM_WORLD);
     }
     
 }
