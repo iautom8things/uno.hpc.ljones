7c9df19a2f4b5256afa0f234843726a7b2ffa022
Manuel Zubieta <mazubieta@gmail.com>
2011-03-15 08:12:33 -0500

    sorry, i had removed the main chunk of code from pib.c when really i justed wanted to comment it out

--- pib.c
+++ pib.c
@@ -127,6 +127,63 @@ int main(int argc, char** argv){
     }
 ///////////////////////////////////////////////////
     setup_tree();
+    /*if (id==0){
+        //start the simualtion
+        printf("Starting the simulation with %d trials\n", NUMBER_OF_TRIALS);
+
+        i = 0;//reset the iterator to use the while loop
+        int unaccepted = 0;
+        int accepted = 0;
+
+
+        //open a file to save the energies, this makes it easier to graph
+        FILE * file = fopen(output_file,"w");
+
+        //do until we have the number of successes we are looking for
+        while(i < NUMBER_OF_TRIALS)
+        {
+            //the energy of the current state of the cube
+            long double old_energy = system_energy(cubes);
+
+            //if the perturb creates an acceptable state then save the energy
+            if(perturb(cubes) == 1)
+            {
+                //write the energy to file
+                fprintf(file,"Energy\t%4d\t%Lf\n",i,old_energy);
+
+                accepted++;
+
+            }//end if
+
+            else
+                unaccepted++;//if not an acceptable state then increment the counter
+
+            i++;//move the iterator
+
+            /////////// Fancy-schmancy progress bar :)
+            int twentieth = NUMBER_OF_TRIALS/20;
+            printf("\r%7d | %3d%% [",i, (i/twentieth)*5);
+            int j;
+            for (j=0;j<i/twentieth;j++){
+                printf("--");
+            }
+
+            for (j=0;j<(20-i/twentieth);j++){
+                printf("  ");
+            }
+            printf("] 100%%");
+            fflush(0);
+            /////////// End fancy-schmancy progress bar
+        }//end while
+
+        //close the file
+        fclose(file);
+
+        //print the successes and the failures
+        printf("\n");
+        printf("Accepted States: %d\n", accepted);
+        printf("Unaccepted States: %d\n" , unaccepted);
+    }//end if main process  */
 ///////////////////////////////////////////////////
     printf("done %d\n", id);
     //do some clean up
